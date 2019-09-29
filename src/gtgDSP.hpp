60093423ee9ceaf2cab6545aff7c8aed8add4ade
Paul Merchant <prmerchant@gmail.com>
2019-09-28 20:32:41 -0700

    add ConstantPanner

--- src/gtgDSP.hpp
+++ src/gtgDSP.hpp
@@ -48,3 +48,22 @@ private:
 	float gain = 1.f;
 
 };
+
+struct ConstantPanner {
+
+	float position = 0.f;
+	float levels[2] = {1.f, 1.f};
+
+	void setPan(float new_position) {
+		if (new_position != position) {
+			position = new_position;
+			float pan_angle = (position + 1.f) * 0.5f;
+			levels[0] = sin((1.f - pan_angle) * M_PI_2) * M_SQRT2;
+			levels[1] = sin(pan_angle * M_PI_2) * M_SQRT2;
+		}
+	}
+
+	float getLevel(int index) {
+		return levels[index];
+	}
+};
