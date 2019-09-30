9097325e3564e154ce7c82e2abb8fa224870b0a1
Paul Merchant <prmerchant@gmail.com>
2019-09-30 16:54:22 -0700

    Gig Bus Mixer upgraded for next release

--- src/GigBus.cpp
+++ src/GigBus.cpp
@@ -47,32 +47,32 @@ struct GigBus : Module {
 
 	void process(const ProcessArgs &args) override {
 
-		// on off button with fader as a pop filter
+		// on off button controls auto fader to avoid pops
 		if (on_trigger.process(params[ON_PARAM].getValue()) + on_cv_trigger.process(inputs[ON_CV_INPUT].getVoltage())) {
 			gig_fader.on = !gig_fader.on;
 		}
 
 		gig_fader.process();
 
-		lights[ON_LIGHT].value = gig_fader.getFade();
-
 		// get input levels
 		float in_levels[3] = {0.f, 0.f, 0.f};
 		in_levels[2] = params[LEVEL_PARAMS + 2].getValue();   // master level
 		for (int sb = 0; sb < 2; sb++) {   // send levels
-			in_levels[sb] = params[LEVEL_PARAMS + sb].getValue() * in_levels[2];   // multiply by red for post levels
+			in_levels[sb] = params[LEVEL_PARAMS + sb].getValue() * in_levels[2];   // multiply by master for post levels
 		}
 
-		// get stereo pan levels
-		if (pan_divider.process()) {   // optimization
+		// get stereo pan levels and set lights
+		if (pan_divider.process()) {   // optimized by checking pan every few samples
 			gig_pan.setPan(params[PAN_PARAM].getValue());
+
+			lights[ON_LIGHT].value = gig_fader.getFade();   // sets lights less frequently
 		}
 
 		// process inputs
 		float stereo_in[2] = {0.f, 0.f};
 		if (inputs[R_INPUT].isConnected()) {   // get a channel from each cable input
-			stereo_in[0] = inputs[LMP_INPUT].getVoltage() * gig_pan.getLevel(1) * gig_fader.getFade();
-			stereo_in[1] = inputs[R_INPUT].getVoltage() * gig_pan.getLevel(2) * gig_fader.getFade();
+			stereo_in[0] = inputs[LMP_INPUT].getVoltage() * gig_pan.getLevel(0) * gig_fader.getFade();
+			stereo_in[1] = inputs[R_INPUT].getVoltage() * gig_pan.getLevel(1) * gig_fader.getFade();
 		} else {   // split mono or sum of polyphonic cable on LMP
 			float lmp_in = inputs[LMP_INPUT].getVoltageSum();
 			for (int c = 0; c < 2; c++) {
@@ -92,7 +92,7 @@ struct GigBus : Module {
 		outputs[BUS_OUTPUT].setChannels(6);
 	}
 
-	// save on button states
+	// save on button and gain states
 	json_t *dataToJson() override {
 		json_t *rootJ = json_object();
 		json_object_set_new(rootJ, "input_on", json_integer(gig_fader.on));
@@ -100,6 +100,7 @@ struct GigBus : Module {
 		return rootJ;
 	}
 
+	// load on button and gain states
 	void dataFromJson(json_t *rootJ) override {
 		json_t *input_onJ = json_object_get(rootJ, "input_on");
 		if (input_onJ) gig_fader.on = json_integer_value(input_onJ);
@@ -107,10 +108,12 @@ struct GigBus : Module {
 		if (gainJ) gig_fader.setGain((float)json_real_value(gainJ));
 	}
 
+	// reset fader speed with new sample rate
 	void onSampleRateChange() override {
 		gig_fader.setSpeed(fade_speed);
 	}
 
+	// reset on state on initialize
 	void onReset() override {
 		gig_fader.on = true;
 		gig_fader.setGain(1.f);
@@ -123,22 +126,22 @@ struct GigBusWidget : ModuleWidget {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/GigBus.svg")));
 
-		addChild(createWidget<ScrewUp>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewUp>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<BlackButton>(mm2px(Vec(10.16, 15.20)), module, GigBus::ON_PARAM));
-		addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(10.16, 15.20)), module, GigBus::ON_LIGHT));
-		addParam(createParamCentered<GrayKnob>(mm2px(Vec(10.127, 60.32)), module, GigBus::PAN_PARAM));
-		addParam(createParamCentered<BlueTinyKnob>(mm2px(Vec(5.91, 72.65)), module, GigBus::LEVEL_PARAMS + 0));
-		addParam(createParamCentered<OrangeTinyKnob>(mm2px(Vec(14.41, 72.65)), module, GigBus::LEVEL_PARAMS + 1));
-		addParam(createParamCentered<RedKnob>(mm2px(Vec(10.188, 85.539)), module, GigBus::LEVEL_PARAMS + 2));
+		addParam(createParamCentered<gtgBlackButton>(mm2px(Vec(10.13, 15.20)), module, GigBus::ON_PARAM));
+		addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(10.13, 15.20)), module, GigBus::ON_LIGHT));
+		addParam(createParamCentered<gtgGrayKnob>(mm2px(Vec(10.13, 60.75)), module, GigBus::PAN_PARAM));
+		addParam(createParamCentered<gtgBlueTinyKnob>(mm2px(Vec(5.4, 73.2)), module, GigBus::LEVEL_PARAMS + 0));
+		addParam(createParamCentered<gtgOrangeTinyKnob>(mm2px(Vec(14.90, 73.2)), module, GigBus::LEVEL_PARAMS + 1));
+		addParam(createParamCentered<gtgRedKnob>(mm2px(Vec(10.13, 86.02)), module, GigBus::LEVEL_PARAMS + 2));
 
-		addInput(createInputCentered<KeyPort>(mm2px(Vec(10.16, 23.233)), module, GigBus::ON_CV_INPUT));
-		addInput(createInputCentered<NutPort>(mm2px(Vec(10.16, 35.583)), module, GigBus::LMP_INPUT));
-		addInput(createInputCentered<NutPort>(mm2px(Vec(10.16, 45.746)), module, GigBus::R_INPUT));
-		addInput(createInputCentered<NutPort>(mm2px(Vec(10.16, 103.863)), module, GigBus::BUS_INPUT));
+		addInput(createInputCentered<gtgKeyPort>(mm2px(Vec(10.13, 23.233)), module, GigBus::ON_CV_INPUT));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(10.13, 35.583)), module, GigBus::LMP_INPUT));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(10.13, 45.746)), module, GigBus::R_INPUT));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(10.13, 103.863)), module, GigBus::BUS_INPUT));
 
-		addOutput(createOutputCentered<NutPort>(mm2px(Vec(10.16, 114.108)), module, GigBus::BUS_OUTPUT));
+		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(10.13, 114.108)), module, GigBus::BUS_OUTPUT));
 	}
 
 	// add gain levels to context menu
@@ -146,7 +149,7 @@ struct GigBusWidget : ModuleWidget {
 		GigBus* module = dynamic_cast<GigBus*>(this->module);
 
 		menu->addChild(new MenuEntry);
-		menu->addChild(createMenuLabel("Gain Level"));
+		menu->addChild(createMenuLabel("Input Gain"));
 
 		struct GainItem : MenuItem {
 			GigBus* module;
@@ -156,7 +159,7 @@ struct GigBusWidget : ModuleWidget {
 			}
 		};
 
-		std::string gainTitles[3] = {"1.0x", "1.5x", "2.0x"};
+		std::string gainTitles[3] = {"100% (default)", "150%", "200%"};
 		float gainAmounts[3] = {1.f, 1.5f, 2.f};
 		for (int i = 0; i < 3; i++) {
 			GainItem* gainItem = createMenuItem<GainItem>(gainTitles[i]);
