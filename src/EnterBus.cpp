111bdf2d10f360d9106cbd26f5995adc214dd0a3
Paul Merchant <prmerchant@gmail.com>
2019-10-14 19:00:45 -0700

    add level knobs to Enter Bus

--- src/EnterBus.cpp
+++ src/EnterBus.cpp
@@ -4,6 +4,7 @@
 
 struct EnterBus : Module {
 	enum ParamIds {
+		ENUMS(LEVEL_PARAMS, 3),
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -21,19 +22,25 @@ struct EnterBus : Module {
 
 	EnterBus() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		configParam(LEVEL_PARAMS + 0, 0.f, 1.f, 1.f, "Blue stereo input level");
+		configParam(LEVEL_PARAMS + 1, 0.f, 1.f, 1.f, "Orange stereo input level");
+		configParam(LEVEL_PARAMS + 2, 0.f, 1.f, 1.f, "Red stereo input level");
 	}
 
 	void process(const ProcessArgs &args) override {
+
+		// process all inputs and levels to bus
+		outputs[BUS_OUTPUT].setVoltage((inputs[ENTER_INPUTS + 0].getVoltage() * params[LEVEL_PARAMS + 0].getValue()) + inputs[BUS_INPUT].getPolyVoltage(0), 0);
+		outputs[BUS_OUTPUT].setVoltage((inputs[ENTER_INPUTS + 1].getVoltage() * params[LEVEL_PARAMS + 0].getValue()) + inputs[BUS_INPUT].getPolyVoltage(1), 1);
+		outputs[BUS_OUTPUT].setVoltage((inputs[ENTER_INPUTS + 2].getVoltage() * params[LEVEL_PARAMS + 1].getValue()) + inputs[BUS_INPUT].getPolyVoltage(2), 2);
+		outputs[BUS_OUTPUT].setVoltage((inputs[ENTER_INPUTS + 3].getVoltage() * params[LEVEL_PARAMS + 1].getValue()) + inputs[BUS_INPUT].getPolyVoltage(3), 3);
+		outputs[BUS_OUTPUT].setVoltage((inputs[ENTER_INPUTS + 4].getVoltage() * params[LEVEL_PARAMS + 2].getValue()) + inputs[BUS_INPUT].getPolyVoltage(4), 4);
+		outputs[BUS_OUTPUT].setVoltage((inputs[ENTER_INPUTS + 5].getVoltage() * params[LEVEL_PARAMS + 2].getValue()) + inputs[BUS_INPUT].getPolyVoltage(5), 5);
+
 		// set output to 3 stereo buses
 		outputs[BUS_OUTPUT].setChannels(6);
-
-		// process all inputs and outputs
-		for (int c = 0; c < 6; c++) {
-			outputs[BUS_OUTPUT].setVoltage(inputs[ENTER_INPUTS + c].getVoltage() + inputs[BUS_INPUT].getPolyVoltage(c), c);
-		}
 	}
 };
-
 
 struct EnterBusWidget : ModuleWidget {
 	EnterBusWidget(EnterBus *module) {
@@ -43,15 +50,19 @@ struct EnterBusWidget : ModuleWidget {
 		addChild(createWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 21.05)), module, EnterBus::ENTER_INPUTS + 0));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 31.2)), module, EnterBus::ENTER_INPUTS + 1));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 46.85)), module, EnterBus::ENTER_INPUTS + 2));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 57.0)), module, EnterBus::ENTER_INPUTS + 3));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 72.85)), module, EnterBus::ENTER_INPUTS + 4));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 83.0)), module, EnterBus::ENTER_INPUTS + 5));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 103.85)), module, EnterBus::BUS_INPUT));
+		addParam(createParamCentered<gtgBlueTinyKnob>(mm2px(Vec(10.37, 34.419)), module, EnterBus::LEVEL_PARAMS + 0));
+		addParam(createParamCentered<gtgOrangeTinyKnob>(mm2px(Vec(10.37, 62.909)), module, EnterBus::LEVEL_PARAMS + 1));
+		addParam(createParamCentered<gtgRedTinyKnob>(mm2px(Vec(10.37, 91.384)), module, EnterBus::LEVEL_PARAMS + 2));
 
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.62, 114.1)), module, EnterBus::BUS_OUTPUT));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(6.87, 15.302)), module, EnterBus::ENTER_INPUTS + 0));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(6.87, 25.446)), module, EnterBus::ENTER_INPUTS + 1));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(6.87, 43.85)), module, EnterBus::ENTER_INPUTS + 2));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(6.87, 53.994)), module, EnterBus::ENTER_INPUTS + 3));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(6.87, 72.354)), module, EnterBus::ENTER_INPUTS + 4));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(6.87, 82.498)), module, EnterBus::ENTER_INPUTS + 5));
+		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.62, 103.863)), module, EnterBus::BUS_INPUT));
+
+		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.62, 114.107)), module, EnterBus::BUS_OUTPUT));
 	}
 };
 
