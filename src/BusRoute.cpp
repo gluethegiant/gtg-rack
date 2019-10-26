f1bbcf09ce5822c3062963e321e147b9a348465f
Paul Merchant <prmerchant@gmail.com>
2019-10-26 00:23:14 -0700

    add Night Ride theme to Bus Route

--- src/BusRoute.cpp
+++ src/BusRoute.cpp
@@ -24,12 +24,14 @@ struct BusRoute : Module {
 	float delay_buf[1000][6] = {};
 	int delay_i = 0;
 	int delay_knobs[3] = {0, 0, 0};
+	int color_theme = 0;
 
 	BusRoute() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(DELAY_PARAMS + 0, 0, 999, 0, "Sample delay on blue bus send");
 		configParam(DELAY_PARAMS + 1, 0, 999, 0, "Sample delay on orange bus send");
 		configParam(DELAY_PARAMS + 2, 0, 999, 0, "Sample delay on red bus send");
+		color_theme = loadDefaultTheme();
 	}
 
 	void process(const ProcessArgs &args) override {
@@ -66,6 +68,20 @@ struct BusRoute : Module {
 		// set output to 3 stereo buses
 		outputs[BUS_OUTPUT].setChannels(6);
 	}
+
+	// save on color theme
+	json_t *dataToJson() override {
+		json_t *rootJ = json_object();
+		json_object_set_new(rootJ, "color_theme", json_integer(color_theme));
+		return rootJ;
+	}
+
+	// load on color theme
+	void dataFromJson(json_t *rootJ) override {
+		json_t *color_themeJ = json_object_get(rootJ, "color_theme");
+		if (color_themeJ) color_theme = json_integer_value(color_themeJ);
+	}
+
 };
 
 // TODO use a single display widget object
@@ -85,7 +101,7 @@ struct BlueDisplay : TransparentWidget {
 		std::string text = string::f("%03d", value);
 
 		// background
-		NVGcolor backgroundColor = nvgRGB(77, 77, 77);
+		NVGcolor backgroundColor = nvgRGB(26, 26, 26);
 		nvgBeginPath(args.vg);
 		nvgRoundedRect(args.vg, 0.0, 0.0, box.size.x, box.size.y, 1.5);
 		nvgFillColor(args.vg, backgroundColor);
@@ -97,7 +113,7 @@ struct BlueDisplay : TransparentWidget {
 		nvgTextLetterSpacing(args.vg, 0.5);
 		nvgTextAlign(args.vg, NVG_ALIGN_RIGHT);
 		Vec textPos = mm2px(Vec(6.05, 3.1));
-		NVGcolor textColor = nvgRGB(250, 250, 223);
+		NVGcolor textColor = nvgRGB(0x90, 0xc7, 0x3e);
 		nvgFillColor(args.vg, textColor);
 		nvgText(args.vg, textPos.x, textPos.y, text.c_str(), NULL);
 	}
@@ -118,7 +134,7 @@ struct OrangeDisplay : TransparentWidget {
 		std::string text = string::f("%03d", value);
 
 		// background
-		NVGcolor backgroundColor = nvgRGB(77, 77, 77);
+		NVGcolor backgroundColor = nvgRGB(26, 26, 26);
 		nvgBeginPath(args.vg);
 		nvgRoundedRect(args.vg, 0.0, 0.0, box.size.x, box.size.y, 1.5);
 		nvgFillColor(args.vg, backgroundColor);
@@ -130,7 +146,7 @@ struct OrangeDisplay : TransparentWidget {
 		nvgTextLetterSpacing(args.vg, 0.5);
 		nvgTextAlign(args.vg, NVG_ALIGN_RIGHT);
 		Vec textPos = mm2px(Vec(6.05, 3.1));
-		NVGcolor textColor = nvgRGB(250, 250, 223);
+		NVGcolor textColor = nvgRGB(0x90, 0xc7, 0x3e);
 		nvgFillColor(args.vg, textColor);
 		nvgText(args.vg, textPos.x, textPos.y, text.c_str(), NULL);
 	}
@@ -151,7 +167,7 @@ struct RedDisplay : TransparentWidget {
 		std::string text = string::f("%03d", value);
 
 		// background
-		NVGcolor backgroundColor = nvgRGB(77, 77, 77);
+		NVGcolor backgroundColor = nvgRGB(26, 26, 26);
 		nvgBeginPath(args.vg);
 		nvgRoundedRect(args.vg, 0.0, 0.0, box.size.x, box.size.y, 1.5);
 		nvgFillColor(args.vg, backgroundColor);
@@ -163,16 +179,26 @@ struct RedDisplay : TransparentWidget {
 		nvgTextLetterSpacing(args.vg, 0.5);
 		nvgTextAlign(args.vg, NVG_ALIGN_RIGHT);
 		Vec textPos = mm2px(Vec(6.05, 3.1));
-		NVGcolor textColor = nvgRGB(250, 250, 223);
+		NVGcolor textColor = nvgRGB(0x90, 0xc7, 0x3e);
 		nvgFillColor(args.vg, textColor);
 		nvgText(args.vg, textPos.x, textPos.y, text.c_str(), NULL);
 	}
 };
 
 struct BusRouteWidget : ModuleWidget {
+	SvgPanel* night_panel;
+
 	BusRouteWidget(BusRoute *module) {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/BusRoute.svg")));
+
+		// load night panel if not preview
+		if (module) {
+			night_panel = new SvgPanel();
+			night_panel->setBackground(APP->window->loadSvg(asset::plugin(pluginInstance, "res/BusRoute_Night.svg")));
+			night_panel->visible = false;
+			addChild(night_panel);
+		}
 
 		BlueDisplay *blueDisplay = createWidgetCentered<BlueDisplay>(mm2px(Vec(15.25, 12.12)));
 		blueDisplay->module = module;
@@ -186,30 +212,77 @@ struct BusRouteWidget : ModuleWidget {
 		redDisplay->module = module;
 		addChild(redDisplay);
 
-		addChild(createWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<gtgScrewUp>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<gtgScrewUp>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createThemedWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, 0), module ? &module->color_theme : NULL));
+		addChild(createThemedWidget<gtgScrewUp>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0), module ? &module->color_theme : NULL));
+		addChild(createThemedWidget<gtgScrewUp>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH), module ? &module->color_theme : NULL));
+		addChild(createThemedWidget<gtgScrewUp>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH), module ? &module->color_theme : NULL));
 
-		addParam(createParamCentered<gtgBlueTinySnapKnob>(mm2px(Vec(15.25, 26.12)), module, BusRoute::DELAY_PARAMS + 0));
-		addParam(createParamCentered<gtgOrangeTinySnapKnob>(mm2px(Vec(15.25, 57.18)), module, BusRoute::DELAY_PARAMS + 1));
-		addParam(createParamCentered<gtgRedTinySnapKnob>(mm2px(Vec(15.25, 88.03)), module, BusRoute::DELAY_PARAMS + 2));
+		addParam(createThemedParamCentered<gtgBlueTinySnapKnob>(mm2px(Vec(15.25, 26.12)), module, BusRoute::DELAY_PARAMS + 0, module ? &module->color_theme : NULL));
+		addParam(createThemedParamCentered<gtgOrangeTinySnapKnob>(mm2px(Vec(15.25, 57.18)), module, BusRoute::DELAY_PARAMS + 1, module ? &module->color_theme : NULL));
+		addParam(createThemedParamCentered<gtgRedTinySnapKnob>(mm2px(Vec(15.25, 88.03)), module, BusRoute::DELAY_PARAMS + 2, module ? &module->color_theme : NULL));
 
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(23.32, 21.05)), module, BusRoute::RETURN_INPUTS + 0));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(23.32, 31.2)), module, BusRoute::RETURN_INPUTS + 1));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(23.32, 52.11)), module, BusRoute::RETURN_INPUTS + 2));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(23.32, 62.23)), module, BusRoute::RETURN_INPUTS + 3));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(23.32, 83.35)), module, BusRoute::RETURN_INPUTS + 4));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(23.32, 93.5)), module, BusRoute::RETURN_INPUTS + 5));
-		addInput(createInputCentered<gtgNutPort>(mm2px(Vec(7.28, 114.1)), module, BusRoute::BUS_INPUT));
+		addInput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(23.32, 21.05)), true, module, BusRoute::RETURN_INPUTS + 0, module ? &module->color_theme : NULL));
+		addInput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(23.32, 31.2)), true, module, BusRoute::RETURN_INPUTS + 1, module ? &module->color_theme : NULL));
+		addInput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(23.32, 52.11)), true, module, BusRoute::RETURN_INPUTS + 2, module ? &module->color_theme : NULL));
+		addInput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(23.32, 62.23)), true, module, BusRoute::RETURN_INPUTS + 3, module ? &module->color_theme : NULL));
+		addInput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(23.32, 83.35)), true, module, BusRoute::RETURN_INPUTS + 4, module ? &module->color_theme : NULL));
+		addInput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(23.32, 93.5)), true, module, BusRoute::RETURN_INPUTS + 5, module ? &module->color_theme : NULL));
+		addInput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(7.28, 114.1)), true, module, BusRoute::BUS_INPUT, module ? &module->color_theme : NULL));
 
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.28, 21.05)), module, BusRoute::SEND_OUTPUTS + 0));
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.28, 31.2)), module, BusRoute::SEND_OUTPUTS + 1));
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.28, 52.11)), module, BusRoute::SEND_OUTPUTS + 2));
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.28, 62.23)), module, BusRoute::SEND_OUTPUTS + 3));
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.28, 83.35)), module, BusRoute::SEND_OUTPUTS + 4));
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(7.28, 93.5)), module, BusRoute::SEND_OUTPUTS + 5));
-		addOutput(createOutputCentered<gtgNutPort>(mm2px(Vec(23.32, 114.1)), module, BusRoute::BUS_OUTPUT));
+		addOutput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(7.28, 21.05)), false, module, BusRoute::SEND_OUTPUTS + 0, module ? &module->color_theme : NULL));
+		addOutput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(7.28, 31.2)), false, module, BusRoute::SEND_OUTPUTS + 1, module ? &module->color_theme : NULL));
+		addOutput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(7.28, 52.11)), false, module, BusRoute::SEND_OUTPUTS + 2, module ? &module->color_theme : NULL));
+		addOutput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(7.28, 62.23)), false, module, BusRoute::SEND_OUTPUTS + 3, module ? &module->color_theme : NULL));
+		addOutput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(7.28, 83.35)), false, module, BusRoute::SEND_OUTPUTS + 4, module ? &module->color_theme : NULL));
+		addOutput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(7.28, 93.5)), false, module, BusRoute::SEND_OUTPUTS + 5, module ? &module->color_theme : NULL));
+		addOutput(createThemedPortCentered<gtgNutPort>(mm2px(Vec(23.32, 114.1)), false, module, BusRoute::BUS_OUTPUT, module ? &module->color_theme : NULL));
+	}
+
+	// add theme items to context menu
+	struct ThemeItem : MenuItem {
+		BusRoute* module;
+		int theme;
+		void onAction(const event::Action& e) override {
+			module->color_theme = theme;
+		}
+	};
+
+	// load default theme
+	struct DefaultThemeItem : MenuItem {
+		BusRoute* module;
+		void onAction(const event::Action &e) override {
+			saveDefaultTheme(rightText.empty());
+		}
+	};
+
+	// build the menu
+	void appendContextMenu(Menu* menu) override {
+		BusRoute* module = dynamic_cast<BusRoute*>(this->module);
+
+		menu->addChild(new MenuEntry);
+		menu->addChild(createMenuLabel("Color Theme"));
+
+		std::string themeTitles[2] = {"70's Cream", "Night Ride"};
+		for (int i = 0; i < 2; i++) {
+			ThemeItem* themeItem = createMenuItem<ThemeItem>(themeTitles[i]);
+			themeItem->rightText = CHECKMARK(module->color_theme == i);
+			themeItem->module = module;
+			themeItem->theme = i;
+			menu->addChild(themeItem);
+		}
+
+		menu->addChild(new MenuEntry);
+		menu->addChild(createMenuLabel("Modular Bus Mixer Defaults"));
+		menu->addChild(createMenuItem<DefaultThemeItem>("Night Ride theme", CHECKMARK(loadDefaultTheme())));
+	}
+
+	// display the panel based on the theme
+	void step() override {
+		if (module) {
+			panel->visible = ((((BusRoute*)module)->color_theme) == 0);
+			night_panel->visible = ((((BusRoute*)module)->color_theme) == 1);
+		}
+		Widget::step();
 	}
 };
 
