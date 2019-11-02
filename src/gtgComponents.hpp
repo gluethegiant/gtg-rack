d3d5933649b349fc44545bff4d25984fd58a4635
Paul Merchant <prmerchant@gmail.com>
2019-11-01 20:30:07 -0700

    add long click button code from Segments

--- src/gtgComponents.hpp
+++ src/gtgComponents.hpp
@@ -87,6 +87,41 @@ struct ThemedSvgScrew : SvgScrew {
 	void step() override;
 };
 
+// long press button, from Audible Instruments Segments (Stages)
+struct LongPressButton {
+	enum Events {
+		NO_PRESS,
+		SHORT_PRESS,
+		LONG_PRESS
+	};
+
+	float pressedTime = 0.f;
+	dsp::BooleanTrigger trigger;
+
+	Events step(Param &param) {
+		Events result = NO_PRESS;
+
+		bool pressed = param.value > 0.f;
+		if (pressed && pressedTime >= 0.f) {
+			pressedTime += APP->engine->getSampleTime();
+			if (pressedTime >= 1.f) {
+				pressedTime = -1.f;
+				result = LONG_PRESS;
+			}
+		}
+
+		// Check if released
+		if (trigger.process(!pressed)) {
+			if (pressedTime >= 0.f) {
+				result = SHORT_PRESS;
+			}
+			pressedTime = 0.f;
+		}
+
+		return result;
+	}
+};
+
 // saving and loading default theme
 
 void saveDefaultTheme(int default_theme);
